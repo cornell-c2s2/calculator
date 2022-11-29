12d6890165ad9dddf8dedbb478887b4d78d5c709
Shihao Cao <shehowcow@gmail.com>
2022-11-29 02:01:53 -0500

    spitting out lots of data

--- src/riscv-startup/main.cpp
+++ src/riscv-startup/main.cpp
@@ -7,9 +7,6 @@
 // #include <vector>
 // #include <iostream>
 // void* __dso_handle;
-#include <string>
-#include <vector>
-#include <cstring>
 
 // #include <metal/machine.h>
 #include <metal/machine/platform.h>
@@ -23,7 +20,8 @@
 
 #include <time.h>       //include Time library (might not need)?
 // #include <iostream>
-#include <RingBuffer.h>
+// #include <RingBuffer.h>
+#define BUFF_SIZE 32
 
 void delay(int number_of_microseconds) //not actually number of seconds
 {
@@ -38,18 +36,7 @@ void delay(int number_of_microseconds) //not actually number of seconds
   
 }//end delay
 
-class HiFiveUart {
-  char buffer[1024];
- 
-  public:
-  HiFiveUart();
-  char get_char();
-  char * get_line();
-};
-
-HiFiveUart::HiFiveUart() {};
-
-char HiFiveUart::get_char() {
+char get_char() {
   volatile uint32_t *reg = (volatile uint32_t *)(METAL_SIFIVE_UART0_0_BASE_ADDRESS + METAL_SIFIVE_UART0_RXDATA);
   uint32_t regval = *reg;
   char regval_c = (char)regval;
@@ -57,42 +44,31 @@ char HiFiveUart::get_char() {
   return regval_c;
 }
 
-char * HiFiveUart::get_line() {
+void read_line(char * buffer, size_t n) {
 
-  int i = 0;
-  for( size_t size = 0; size < 1024; size++ ) {
+  // int i = 0;
+  // char buffer[1024];
+  size_t size = 0;
+
+  while( true ) {
     // printf("size: %u\n", size);
+    if( size >= n) {
+      break;
+    }
+
     char c = get_char();
-    // buffer[size] = c;
+    if (c != 0 ) {
+      printf("Got char: %c", c);
+      buffer[size] = c;
+      size++;
+    }
+
+    if (c == '\n'){
+      break;
+    }
   }
-  
-  // while( true ) {
-  //   printf("size: %u\n", size);
-  //   if ( size == 1024 ) {
-  //     break;
-  //   }
+}
 
-  //   // char c = '0';    
-    char c = get_char();
-
-  //   if ( c != 0 ) {
-  //     buffer[size] = c;
-  //     size++;
-  //   }
-
-  //   if ( c == '\n' ) {
-  //     break;
-  //   }
-  // }
-
-  char * ret = "asdf";
-  return ret;
-
-  // char * line_buf = (char*) malloc( size * sizeof(char) );
-  // std::memcpy(line_buf, &buffer, size);
-
-  // return line_buf;
-};
 
 // void return_foo(std::string& ref) {
 //   // ref = "asdf";
@@ -100,8 +76,8 @@ char * HiFiveUart::get_line() {
 
 int main() {
   char buffer[20] = {};
-  char * buff_pointer = &buffer[0];
-  size_t buff_size = 20;
+  // char * buff_pointer = &buffer[0];
+  // size_t buff_size = 20;
 
   // for(size_t i = 0;)
   for(int i = 0; i < 6; i++) {
@@ -116,49 +92,12 @@ int main() {
     printf(FOOBAR);
     #endif
   }
-
-  HiFiveUart uart = HiFiveUart();
   
   while(true) {
     delay(1000*1);
-    // std::string test = "Finally\n";
-    // std::vector<char> foo{80};
-    // const char[] chars = {foo[0]};
-    // printf("Got input: ");
-    // getline(&buff_pointer, &buff_size, stdin);
-    // printf(stdin);
-    // scanf("%19s", buffer);
-
-    // int read_int = 0;
-    // if( metal_tty_getc(&read_int) == 0) {
-    //   buffer[0] = (char)(read_int);
-    //   buffer[1] = 0x00;
-
-    // } else {
-    //   buffer[0] = 0x00;
-    //   // break;
-    // }
-
-    // volatile uint32_t *reg = (volatile uint32_t *)(METAL_SIFIVE_UART0_0_BASE_ADDRESS + METAL_SIFIVE_UART0_RXDATA);
-    // uint32_t regval = *reg;
-    // if (!(regval & (1<<31))){
-    //   buffer[0] = (char)((regval) & 0xff);
-    //   buffer[1] = 0x00;
-
-    // } else {
-    //   buffer[0] = 0x00;
-    //   // break;
-    // }
-
-    // std::getline(std::cin, test);
-    // printf(test.c_str());
-    // printf("PREPRINT\n");
-    // printf(test.c_str());
-    // std::string line = return_foo();
-    // std::string line = "foo";
-    char * line = uart.get_line();
-    // char * line = "uart";
-    printf("%s", line);
+    char line_buf[BUFF_SIZE];
+    read_line(line_buf, BUFF_SIZE);
+    printf("%s", line_buf);
     fflush(stdout);
   
     // printf("aESTING\n");
